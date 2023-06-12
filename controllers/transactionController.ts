f847d6da6f0f1f230b4e8d88332b2de8580d416d
William Huang <william.huang001@binus.ac.id>
2023-06-12 16:55:51 +0900

    add delete transaction api

--- controllers/transactionController.ts
+++ controllers/transactionController.ts
@@ -1,71 +1,76 @@
-import { Request, Response } from 'express';
-import asyncHandler from 'express-async-handler';
-import Transaction from '../models/transactionModel';
-import mongoose from 'mongoose';
-import axios from 'axios';
+import { Request, Response } from "express";
+import asyncHandler from "express-async-handler";
+import Transaction from "../models/transactionModel";
+import mongoose from "mongoose";
+import axios from "axios";
 interface RequestWithUser extends Request {
-    user: mongoose.Document | null;
+	user: mongoose.Document | null;
 }
 
 // Desc: add a transaction
 // Route : /api/transactions/add
-// access : public
-const addTransaction = asyncHandler(async (req: Request, res: Response) => {    
-    const {name, item, category, amount} = req.body;
-    //Create transaction
-    await Transaction.create({
-        name: name,
-        item: item,
-        category: category,
-        amount: amount,
-        userId: (req as RequestWithUser).user?._id
-    });
-    res.json({message: "Successfully added!"});
-}); 
+// access : private
+const addTransaction = asyncHandler(async (req: Request, res: Response) => {
+	const { name, item, category, amount } = req.body;
+	//Create transaction
+	await Transaction.create({
+		name: name,
+		item: item,
+		category: category,
+		amount: amount,
+		userId: (req as RequestWithUser).user?._id,
+	});
+	res.json({ message: "Successfully added!" });
+});
 
 // Desc: get transactions
 // Route : /api/transactions/get
 // access : private
-const getTransaction = asyncHandler(async (req: Request, res: Response) => {    
-    //get all transactions
-    const transactions = await Transaction.find({userId: (req as RequestWithUser).user?._id});
+const getTransaction = asyncHandler(async (req: Request, res: Response) => {
+	//get all transactions
+	const transactions = await Transaction.find({
+		userId: (req as RequestWithUser).user?._id,
+	});
 
-    // Send transactions in response
-    res.json(transactions);
-}); 
+	// Send transactions in response
+	res.json(transactions);
+});
 
 // Desc: classify transactions
 // Route : /api/transactions/classify
 // access : private
-const classifyTransaction = asyncHandler(async (req: Request, res: Response) => {    
-    const { text } = req.body;
+const classifyTransaction = asyncHandler(
+	async (req: Request, res: Response) => {
+		const { text } = req.body;
 
-  const categories = [
-    "Groceries",
-    "Eating Out",
-    "Transportation",
-    "Utilities",
-    "Rent",
-    "Entertainment",
-    "Healthcare",
-    "Personal",
-    "Clothing",
-    "Electronics",
-    "Education",
-    "Travel",
-    "Gifts & Donations",
-    "Insurance",
-    "Investments",
-    "Miscellaneous"
-  ];
+		const categories = [
+			"Groceries",
+			"Eating Out",
+			"Transportation",
+			"Utilities",
+			"Rent",
+			"Entertainment",
+			"Healthcare",
+			"Personal",
+			"Clothing",
+			"Electronics",
+			"Education",
+			"Travel",
+			"Gifts & Donations",
+			"Insurance",
+			"Investments",
+			"Miscellaneous",
+		];
 
-  try {
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content: ` 
+		try {
+			const response = await axios.post(
+				"https://api.openai.com/v1/chat/completions",
+				{
+					model: "gpt-3.5-turbo",
+					messages: [
+						{
+							role: "system",
+							content: ` 
           You are an intelligent assistant that classifies expenses into categories. Here are some rules to guide your responses:
 
           1. We have a list of categories: ${categories.join(", ")}.
@@ -75,44 +80,63 @@ const classifyTransaction = asyncHandler(async (req: Request, res: Response) => 
           3. If the input could potentially fall into multiple categories, make an educated guess based on the information available.
           
           4. If the input doesn't provide a valid amount spent, fill it in as "Unknown" or "0".
-          `
-        },
-        {
-          role: "user",
-          content: text
-        },
-        {
-          role: "assistant",
-          content: `
+          `,
+						},
+						{
+							role: "user",
+							content: text,
+						},
+						{
+							role: "assistant",
+							content: `
 
-          Please classify the expense into one of the categories and generate a JSON object with the following fields (Use capitalize format): \n1. name (2 words or more of summarization with clear action context of the text and establishment's name, if it is not about expenses or spending, fill it as "Unknown")\n2. item (the item purchased)\n3. category (the best fitting category from the list)\n4. amount (the amount spent, just number do not include currency character )\n\nJSON:`
-        }
-      ],
-      temperature: 0.3,
-      max_tokens: 60,
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    });
+          Please classify the expense into one of the categories and generate a JSON object with the following fields (Use capitalize format): \n1. name (2 words or more of summarization with clear action context of the text and establishment's name, if it is not about expenses or spending, fill it as "Unknown")\n2. item (the item purchased)\n3. category (the best fitting category from the list)\n4. amount (the amount spent, just number do not include currency character )\n\nJSON:`,
+						},
+					],
+					temperature: 0.3,
+					max_tokens: 60,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+						"Content-Type": "application/json",
+					},
+				}
+			);
 
-    const result = response.data.choices[0].message.content.trim().split('\n');
+			const result = response.data.choices[0].message.content
+				.trim()
+				.split("\n");
 
+			// Parse the result to fit into your desired JSON format
 
-    // Parse the result to fit into your desired JSON format
+			const parsedResult = JSON.parse(result.join(""));
 
-    const parsedResult = JSON.parse(result.join(''));
+			res.json(parsedResult);
+		} catch (error) {
+			console.error(error);
+			res.status(500).send("Error in OpenAI API call");
+		}
+	}
+);
 
-    res.json(parsedResult);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error in OpenAI API call');
-  }
-}); 
+// Desc: add a transaction
+// Route : /api/transactions/delete
+// access : private
+const deleteTransaction = asyncHandler(async (req: Request, res: Response) => {
+	const { id } = req.body;
+	try {
+		//delete transaction
+		await Transaction.deleteOne({ _id: id });
+		res.json({ message: "Successfully deleted!" });
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
 
-export{
-    addTransaction,
-    getTransaction,
-    classifyTransaction
-}
+export {
+	addTransaction,
+	getTransaction,
+	classifyTransaction,
+	deleteTransaction,
+};
--- routes/transactionRoutes.ts
+++ routes/transactionRoutes.ts
@@ -1,14 +1,19 @@
-import express from 'express';
-import dotenv from  'dotenv';
-import { addTransaction, classifyTransaction, getTransaction } from '../controllers/transactionController';
+import express from "express";
+import dotenv from "dotenv";
+import {
+	addTransaction,
+	classifyTransaction,
+	deleteTransaction,
+	getTransaction,
+} from "../controllers/transactionController";
 dotenv.config();
-import { authUser } from '../middlewares/authMiddleware';
+import { authUser } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
-router.post('/add', authUser, addTransaction);
-router.get('/get', authUser, getTransaction);
-router.post('/classify', authUser, classifyTransaction);
-
+router.post("/add", authUser, addTransaction);
+router.get("/get", authUser, getTransaction);
+router.post("/classify", authUser, classifyTransaction);
+router.delete("/delete", authUser, deleteTransaction);
 
 module.exports = router;
